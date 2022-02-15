ffbc73da4a24de4f1ef36ca5d9c1bbecd944fb0e
Jakub <kubhinio@gmail.com>
2022-02-15 10:18:46 +0100

    inital commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,63 @@
+import { Component } from '@angular/core';
+import { Task } from './task';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  config: { [key: string]: string} | null = null;
+
+  //taskName: string;
+  tasks: Task[] = [
+    {
+      name: 'Siłownia',
+      deadline: '2022-02-20',
+      done: false
+    },
+    {
+      name: 'Nauka angulara',
+      deadline: '2022-02-21',
+      done: false
+    },
+    {
+      name: 'Sprzątanie mieskzania',
+      deadline: '2022-02-22',
+      done: false
+    }
+  ];
+
+  constructor() {
+    setTimeout( () => {
+      this.config = {
+        title: 'Lista zadań',
+        footer: ' Lista zadań zbudowana w angularze',
+        date: new Date().toDateString()
+      };
+    }, 500);
+  }
+
+  clearTasks() {
+    this.tasks = [];
+  }
+
+  // onKeyUp(event: KeyboardEvent ){
+  //   const target = event.target as HTMLInputElement;
+  //   //console.log(target.value);
+  //   this.taskName = target.value;
+  // }
+
+  createTask(name: string, deadline: string){
+    const task: Task = {
+      //name: this.taskName,
+      name,
+      //name: name,
+      //deadline: '2022-02-14',
+      deadline,
+      //deadline: date,
+      done: false
+    };
+    this.tasks.push(task);
+  }
+}
